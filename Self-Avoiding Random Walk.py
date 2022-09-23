a4245283e78afbecb1fe27e00cc33028569387b9
Fabrizio Martone <53048926+Blue00FF@users.noreply.github.com>
2022-09-23 15:20:46 +0100

    Added fixed grid
    Debugged a problem with the grid not being passed as an argument to the check position function which arose after wrapping the main code into the "main()" function. Added fixed limits to show the whole grid in the graph every time. Corrected the y offset to make the graph the same as the 2d array.

--- Self-Avoiding Random Walk.py
+++ Self-Avoiding Random Walk.py
@@ -10,7 +10,10 @@ from numpy.typing import ArrayLike
 GRID_DIMENSION: int = 5
 
 
-def check_possible_direction(current_pos: Collection[int]) -> None:
+def check_possible_direction(
+        current_pos: Collection[int],
+        grid: Collection[Collection[int]]
+    ) -> None:
     """
     Function Taking the numpy array expressing the current position as input
     and modifying the corresponding Spot object on the grid to reflect the
@@ -43,13 +46,12 @@ class Spot:
     - Which directions we can choose for our next move.
     - If the point has been visited or not.
     """
-    
+
     def __init__(self):
-        self.options: Dict = {"up": True, "down": True, "left": True,
-                              "right": True}
+        self.options: Dict = {"up": True, "down": True, "left": True, "right": True}
         self.allowed: List = []
         self.visited: bool = False
-    
+
     def generate_allowed(self) -> None:
         """
         From the self.options dictionary, filters the directions which
@@ -70,13 +72,13 @@ def main():
         for j in range(GRID_DIMENSION):
             grid[i].append(Spot())
     grid = np.array(grid)
-    
+
     # Initialise the current position as the upper left corner of the grid and
     # the history, i.e. the list which is going to contain the positions taken
     # in our random walk.
     current_position: Collection[int] = np.array([0, 0])
     history = []
-    
+
     while True:
         # Add the current position to the history.
         history.append(current_position.copy())
@@ -87,7 +89,7 @@ def main():
         current_grid_pos.visited = True
         # Update the possible directions in the object describing the current
         # position on the grid.
-        check_possible_direction(current_position)
+        check_possible_direction(current_position, grid)
         current_grid_pos.generate_allowed()
         # Choose a new direction to go at random (if there is one), else exit
         # the loop. If the walk has gone across the whole grid it will print
@@ -108,23 +110,25 @@ def main():
         else:
             print("Stuck!")
             break
-    
+
     # Convert the history list into an array for convenience.
     history = np.array(history)
-    
+
     # Split the array into the x coordinates and the y coordinates.
     x: Collection[int] = history[:, 0]
     y: Collection[int] = history[:, 1]
-    
+
     # Flip the y-axis in order to maintain the correspondence between the grid,
     # where (0,0) is the upper left corner, and the plot graph.
-    y = abs(y - 5)
-    
+    y = abs(y - 4)
+
     # Plot the random walk result on a graph, marking the starting position
     # with a
     # green dot, the ending position with a red dot and the intermediate
     # ones with
     # blue dots.
+    plt.xlim(-0.1, 4.1)
+    plt.ylim(-0.1, 4.1)
     plt.plot(x, y)
     plt.plot(x, y, "bo")
     plt.plot(x[0], y[0], "go")
